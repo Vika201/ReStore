f97b0008d7ddd423d21e7e4eb291b51ab3417fd9
Viktoriia <80117510+Vika201@users.noreply.github.com>
2022-07-29 12:59:45 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import App from './components/app';
 
-ReactDOM.render(<App/>,
-    document.getElementById('root'));
+import BookstoreService from './services/bookstore-service';
+import { BookstoreServiceProvider } from "./components/bookstore-service-context";
+import ErrorBoundry from './components/error-boundry';
+
+import store from './store';
+
+const bookstoreService = new BookstoreService();
+
+ReactDOM.render(
+  <Provider store={store}>
+      <ErrorBoundry>
+          <BookstoreServiceProvider value={bookstoreService}>
+              <Router>
+                  <App />
+              </Router>
+          </BookstoreServiceProvider>
+      </ErrorBoundry>
+  </Provider>,
+  document.getElementById('root')
+);
